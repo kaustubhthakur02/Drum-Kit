927e6b470e96a2dac36f66a69720c693026a26fd
kaustubhthakur02 <154906228+kaustubhthakur02@users.noreply.github.com>
2024-04-18 13:49:04 +0530

    Files uploaded

---
+++ index.js
@@ -0,0 +1,52 @@
+let btn = document.querySelectorAll(".drum").length
+
+for (let i = 0; i < btn; i++){
+
+     document.querySelectorAll(".drum")[i].addEventListener("click", function(){
+       let buttonInnerHTML = this.innerHTML;
+        makesound(buttonInnerHTML)
+     });
+
+
+    }
+ document.addEventListener("keypress", function (event){
+
+    makesound(event.key)
+})
+
+function makesound(key){
+    switch (key) {
+        case "w":
+            let audio1 = new Audio('sounds/crash.mp3')
+            audio1.play();
+            break;
+        case "a":
+            let audio2 = new Audio('sounds/kick-bass.mp3')
+            audio2.play();
+            break;
+        case "s":
+            let audio3 = new Audio('sounds/snare.mp3')
+            audio3.play();
+            break;
+        case "d":
+            let audio4 = new Audio('sounds/tom-1.mp3')
+            audio4.play();
+            break;
+        case "j":
+            let audio5 = new Audio('sounds/tom-2.mp3')
+            audio5.play();
+            break;
+        case "k":
+            let audio6 = new Audio('sounds/tom-3.mp3')
+            audio6.play();
+            break;
+        case "l":
+            let audio7 = new Audio('sounds/tom-4.mp3')
+            audio7.play();
+            break;
+        default:
+       }
+
+}
+
+    
